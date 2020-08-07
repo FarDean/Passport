938ba41367891a75400de128bb1762b62d1b6f59
FarDean <sfh1212@gmail.com>
2020-08-07 15:40:07 +0430

    until hashing password

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,62 @@
 const express = require('express');
 const router = express.Router();
-
+const User = require('../models/User');
+const bcrypt = require('bcryptjs')
 router.get('/login',(req,res)=>{
-    res.send('login')
-})
+    res.render('login')
+});
 router.get('/register',(req,res)=>{
-    res.send('register')
+    res.render('register')
+});
+
+router.post('/register',(req,res)=>{
+    const {name,email,password,password2} =req.body;
+    let errors = [];
+
+    if(!name || !email || !password || !password2){
+        errors.push({msg:'please fill in all fields'})
+    }
+    if(password !== password2){
+        errors.push({msg:'password do not match'});
+    }
+    if(password.length < 8){
+        errors.push({msg:"password should be at least 8 characters"});
+
+    }
+    if(errors.length>0){
+        res.render('register',{
+            errors,
+            name,
+            email,
+            password,
+            password2
+        })
+    }else{
+        User.findOne({email:email}).then(user =>{
+            if(user){
+                errors.push({msg: 'email already exists'})
+                res.render('register',{
+                    errors,
+                    name,
+                    email,
+                    password,
+                    password2
+                })
+            }else{
+                const newUser = new User({
+                    name,
+                    email,
+                    password
+                });
+                bcrypt.genSalt(10,(err,salt)=>bcrypt.hash(newUser.password,salt,(err,hash)=>{
+                    if(err) throw err;
+
+                    newUser.password = hash;
+                    newUser.save().then(user =>{res.redirect('/users/login')}).catch(err=> console.log(err));
+                }))
+            }
+        })
+    }
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const app = express();
-const path =require('path')
-const indexRouter = require('./routes/index')
+const mongoose = require('mongoose');
+const path =require('path');
+const indexRouter = require('./routes/index');
 const userRouter =require('./routes/users');
 const expressLayouts = require('express-ejs-layouts');
-app.use(expressLayouts)
-app.set('view engine', 'ejs')
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.set('layout', 'layout/layout');
-app.use('/',indexRouter)
-app.use('/users', userRouter)
-app.use(express.static('public'))
+app.use(express.urlencoded({extended:false}));
+mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true,useUnifiedTopology: true});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('connected to mongoose...')
+});
+app.use('/',indexRouter);
+app.use('/users', userRouter);
+app.use(express.static('public'));
 
 
 const PORT = process.env.PORT || 3000;
